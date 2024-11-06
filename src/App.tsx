f1c9ca698a1dfb23799ea2e25d2f4a643b943bd6
Yosi <Yosi@YosiWebiks.com>
2024-11-06 10:02:06 +0200

    create comps and some css grid basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { approveClaim } from './redux/store'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { approveClaim } from "./redux/store";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Page from "./components/pages/Page";
 
 export default function App() {
-  const treasure = useSelector((st:any)=>st.treasure)
- 
-  
   return (
-    <div>App {treasure}</div>
-  )
+    <div className="app">
+      <header>
+        <Header />
+      </header>
+      <aside>
+        <Nav />
+      </aside>
+      <main>
+        <Page />
+      </main>
+    </div>
+  );
 }
