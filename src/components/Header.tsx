4877a3233bc9cea6f10a0266f5f8642cddded630
Yosi <Yosi@YosiWebiks.com>
2024-11-06 16:17:04 +0200

    some style + display ther header stats

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Claim } from "../types/redux";
 
 export default function Header() {
+  const treasure = useSelector((state: any) => state.treasure);
+  const claims = useSelector((state: any) => state.claims);
+  const policies = useSelector((state: any) => state.policies);
+
+  const calcRatio = () => {
+    const sum = claims.length;
+    const approved = claims.filter((c: Claim) => c.approved).length;
+    return (100 * approved) / sum;
+  };
+
   return (
-    <div className='full'>Header</div>
-  )
+    <div className="full">
+      <h1>${treasure.toFixed(2)}</h1>
+      <div className="stats">
+        <div className="customers">
+          <h1>{policies.length}</h1>
+          <h4>Active customers</h4>
+        </div>
+        <div className="open-claims">
+          <h1>{claims.length}</h1>
+          <h4>Open Claims</h4>
+        </div>
+        <div className="approved">
+          <h1>{calcRatio()}%</h1>
+          <h4>Approved ratio</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/pages/ListDisplay.tsx
+++ src/components/pages/ListDisplay.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { PageType } from '../../types/enums'
+import { useSelector } from 'react-redux'
+import { Claim, Policy } from '../../types/redux'
 
 interface Props {
     pageType:PageType
 }
 
 export default function ListDisplay({pageType}:Props ){
+    // const arr:Claim[]|Policy[] = useSelector((s:any)=>s[pageType.toLowerCase()])
   return (
     <div>
         ListDisplay ft. {pageType.toLowerCase()}
 
-        {/* { ([].map((it:any)=>{
+        {/* { (arr.map((it:any)=>{
             if (pageType == PageType.CLAIMS) {
                 return
             }else{
